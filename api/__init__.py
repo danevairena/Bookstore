736e054f09ba94035b679456efac436ac0911d76
Irena Daneva <de.luxe9997@gmail.com>
2023-06-01 15:16:47 +0300

    update

--- api/__init__.py
+++ api/__init__.py
@@ -2,11 +2,19 @@ from flask import Flask
 from config import Config
 # SQLAlchemy package is an Object Relational Mapper(ORM), that allows the application to manage 
 # a database using high-level entities such as classes, objects and methods instead of tables and SQL.
+# The job of the ORM is to translate the high-level operations into database commands.
 from flask_sqlalchemy import SQLAlchemy
+# Relational databases are centered around structured data, so when the structure changes 
+# the data that is already in the database needs to be migrated to the modified structure
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 # Tell Flask to read and apply the config file
 app.config.from_object(Config)
 
-from api import routes
+# The database is going to be represented in the application by the database instance. 
+# The database migration engine will also have an instance. 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from api import routes, models
--- config.py
+++ config.py
@@ -3,6 +3,13 @@
 # The Flask-WTF extension uses the secret key to protect web forms against a nasty attack called Cross-Site Request Forgery
 
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    # The Flask-SQLAlchemy extension takes the location of the application's database from the SQLALCHEMY_DATABASE_URI configuration variable.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Sending a signal to the application every time a change is about to be made in the database is disabled
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
