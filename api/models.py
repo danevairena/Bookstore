da6de954ecc4515186b15b3c1424d657833bc46c
Irena Daneva <de.luxe9997@gmail.com>
2023-06-02 09:14:05 +0300

    update

--- api/models.py
+++ api/models.py
@@ -1,8 +1,26 @@
 from datetime import datetime
 from api import db
+# Werkzeug implements password hashing - the password is transformed into a long encoded string 
+# through a series of cryptographic operations that have no known reverse operation, which means 
+# that a person that obtains the hashed password will be unable to use it to obtain the original password.
+# The verification process is done with a second function from Werkzeug
+from werkzeug.security import generate_password_hash, check_password_hash
+# Flask-Login provides class called UserMixin that includes generic implementations that are appropriate for most user model classes
+from flask_login import UserMixin
+from api import login
+
+# Flask-Login knows nothing about databases, so the extension expects that the application will 
+# configure a user loader function, that can be called to load a user given the ID.
+# The user loader is registered with Flask-Login with the @login.user_loader decorator. 
+# The id that Flask-Login passes to the function as an argument needs to be a string
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 
 # The User class inherits from db.Model, a base class for all models from Flask-SQLAlchemy. 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -18,6 +36,15 @@ class User(db.Model):
     # The __repr__ method tells Python how to print objects of this class
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    # With the two methods below, a user object is now able to do secure password verification, 
+    # without the need to ever store original passwords.
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 # The new Post class will represent listings posted by users   
 class Post(db.Model):
--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,8 @@
 from api import app
 from api.forms import LoginForm
 from flask import redirect, url_for
+from flask_login import current_user, login_user
+from api.models import User
 
 @app.route('/')
 
@@ -13,6 +15,24 @@ def index():
 # The methods argument in the route decorator tells Flask that this view function 
 # accepts GET and POST requests, overriding the default, which is to accept only GET requests.
 @app.route('/login', methods=['GET', 'POST'])
+def login():
+    # If user is logged in avoid navigating to /login page
+    # The current_user variable comes from Flask-Login and can be used at any time during the handling to obtain 
+    # the user object that represents the client of the request. The value of this variable can be a user object from the database, 
+    # which Flask-Login reads through the user loader callback or a special anonymous user object if the user did not log in yet.
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    # TODO ---------------
+    return 
+
+
 
 # view function that accepts and validates the data submitted by the user
 def login():
@@ -26,4 +46,5 @@ def login():
         # url_for() generates an application URL. It uses use the function names instead of URLs.
         # Some URLs have dynamic components in them, so generating those URLs by hand may cause errors
         return redirect(url_for('index'))
+    # TODO --------------
     return 404
