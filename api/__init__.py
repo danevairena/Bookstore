6dade00788d4be36673d1e2abdb867ca3ae706c2
Irena Daneva <de.luxe9997@gmail.com>
2023-06-02 09:14:10 +0300

    update

--- api/__init__.py
+++ api/__init__.py
@@ -7,11 +7,16 @@ from flask_sqlalchemy import SQLAlchemy
 # Relational databases are centered around structured data, so when the structure changes 
 # the data that is already in the database needs to be migrated to the modified structure
 from flask_migrate import Migrate
+# Flask-Login extension manages the user logged-in state, so that for example users can log in to the application 
+# and then navigate to different pages while the application "remembers" that the user is logged in.
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 # Tell Flask to read and apply the config file
 app.config.from_object(Config)
+# As with other extensions, Flask-Login needs to be created and initialized
+login = LoginManager(app)
 
 # The database is going to be represented in the application by the database instance. 
 # The database migration engine will also have an instance. 
