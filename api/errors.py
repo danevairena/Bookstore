3ae333c484f8ca77d3e07a3fb0d49b04aea653b1
Irena Daneva <de.luxe9997@gmail.com>
2023-06-09 14:19:44 +0300

    error handling

--- api/errors.py
+++ api/errors.py
@@ -1,21 +1,19 @@
-from api import app, db
+# Error responses
 
-# Flask provides a mechanism for an application to install its own error pages, 
-# so that your users don't have to see the plain and boring default ones.
-# To declare a custom error handler, the @errorhandler decorator is used.
+from flask import jsonify
+# The HTTP_STATUS_CODES dictionary from Werkzeug (a core dependency of Flask) provides a short descriptive name for each HTTP status code.
+from werkzeug.http import HTTP_STATUS_CODES
 
-@app.errorhandler(404)
-def not_found_error(error):
-    # TODO --------
-    return 404
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+        # The jsonify() function returns a Flask Response object with a default status code of 200, 
+        # so after I create the response, I set the status code to the correct one for the error
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
 
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    # TODO --------
-    return 500
-
-# Error handling placeholder.
-def bad_request():
-    pass
+# The most common error that the API is going to return is going to be the code 400, which is the error for "bad request"
+def bad_request(message):
+    return error_response(400, message)
