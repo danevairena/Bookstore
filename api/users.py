df69ea5e1bd64574fa7259130fc4c1217baaa927
Irena Daneva <de.luxe9997@gmail.com>
2023-06-09 14:35:03 +0300

    update

--- api/users.py
+++ api/users.py
@@ -1,10 +1,14 @@
-from flask import app
+from flask import jsonify
+from api import app
+from api.models import User
 
-# User API resource placeholders.
-
+# Retrieve a single user, given by id
+# The view function receives the id for the requested user as a dynamic argument in the URL.
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    pass
+    # The advantage of get_or_404() over get() is that it removes the need to check the result of the query -
+    # when the id does not exist, it aborts the request and returns a 404 error to the client.
+    return jsonify(User.query.get_or_404(id).to_dict())
 
 @app.route('/users', methods=['GET'])
 def get_users():
